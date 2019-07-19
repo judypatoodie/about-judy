c3de744e55d21306f31355c0091fed88f2d2037e
Judy Huynh <jhuynh@mossbeachhomes.com>
2019-07-19 15:13:25 -0700

    added scrolling effect

--- jqueryEffects.js
+++ jqueryEffects.js
@@ -3,6 +3,27 @@ window.onload = function() {
   $('.overlay').hide().fadeIn(2000);
   $('#subOverlay').hide().fadeIn(5000);
 }
+$("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
 
 $(window).scroll(function() {
   // checks if window is scrolled more than 500px, adds/removes solid class
